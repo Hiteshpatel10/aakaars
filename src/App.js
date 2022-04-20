16c478a104275c0be0ae683634b4a3abfb2c9cd8
Hitesh Patel <hpatel.hp2001@gmail.com>
2022-04-20 15:46:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Redirect, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/Navbar'
+import Home from './pages/home'
+import Contacts from './pages/contacts'
+import About from './pages/about'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<About />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import phimg from "../assets/placeholderimg.jpg";
+import "./ItemCard.css";
+
+export default function ItemCard() {
+  return (
+    <div className="card outer-card shadow-sm mb-5 bg-white rounded">
+      <img className="card-img-top" src={phimg} alt="Card image cap" />
+
+      <div className="card-body">
+        <h5 className="card-title text-center">Card title</h5>
+      </div>
+    </div>
+  );
+}
