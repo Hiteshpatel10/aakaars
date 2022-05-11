31aa0dcbe4221090b33e638e19c30be6c0dceb5a
Hitesh Patel <hpatel.hp2001@gmail.com>
2022-05-11 15:00:43 +0530

    our products list added

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import phimg from "../assets/placeholderimg.jpg";
+import images from "../assets/images";
 import "./ItemCard.css";
 
 export default function ItemCard() {
   return (
-    <div className="outer-card">
-      <div className="card outer-card shadow-sm mb-5 bg-white rounded">
-        <img className="card-img-top" src={phimg} alt="Card image cap" />
-
-        <div className="card-body">
-          <h5 className="card-title text-center">Card title</h5>
-        </div>
+    <div className="container">
+      <h3 className="itemcard-title">Our Products</h3>
+      <div className="row">
+        {images.map((item) => (
+          <div className="col-sm">
+            <div className="card outer-card shadow-sm mb-5 bg-white rounded">
+              <img
+                className="card-img-top"
+                src={item.src}
+                alt="Card image cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">{item.alt}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
